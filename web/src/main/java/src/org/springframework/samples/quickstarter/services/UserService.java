82d66a0f575afd9a692fb7f53c1eda94c9cc2ca2
EC2 Default User <ec2-user@ip-172-31-23-108.ap-southeast-1.compute.internal>
2015-08-16 16:21:27 +0000

    initial quickstart code commit

---
+++ web/src/main/java/src/org/springframework/samples/quickstarter/services/UserService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.springframework.samples.quickstarter.services;
+
+import org.springframework.samples.quickstarter.model.User;
+
+/**
+ * @author AM283065
+ *
+ */
+public interface UserService {
+
+	public User getUserDetails();
+}
---
+++ web/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Sample Application</title>
+  </head>
+  <body>
+	<h1>Hello, ${user.firstName} ${user.lastName}!</h1>
+  </body>
+</html>
